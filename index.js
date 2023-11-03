f8067e659d43123404c1435c6f671eab05c5a228
abdulqader <abdulqaderpatel2002@gmail.com>
2023-11-03 16:15:41 +0530

    jkljkl

--- index.js
+++ index.js
@@ -10,24 +10,21 @@ app.get(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-app.get("/hello",(req,res)=>{
-    res.json("hello")
-})
+const connectMongo=require("./connectMongo")
 
-app.post('/', async(req, res) => {
-    const {name,email,password}=req.body
-    const user=new User({name,email,password})
-    await user.save()
-    res.json("data added")
+connectMongo()
+
+app.get("/hello", (req, res) => {
+  res.json("hello");
 });
 
+app.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password });
+  await user.save();
+  res.json("data added");
+});
 
-
-mongoose
-        .connect(process.env.MONGODB_CONNECT_URI)
-        .then(() => {   
-            app.listen(process.env.PORT, (() => {
-            console.log(`server running`);
-        }))
-    }) 
-    .catch((error) => console.log(error));
+app.listen(process.env.PORT, () => {
+  console.log(`server running`);
+});
